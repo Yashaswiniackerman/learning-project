e37b947cece13e235639bff3d299c6d671387b3f
Yashaswiniackerman <yashaswiniky77@gmail.com>
2022-05-25 17:26:14 +0530

    commit

---
+++ LeaningProject/src/StudentConstructor.java
@@ -0,0 +1,31 @@
+
+
+public class StudentConstructor {
+	int id;
+	String name;
+	String course;
+	String email;
+	int phn;
+	
+	public StudentConstructor(int id, String name, String course, String email, int phn) {
+		this.id=id;
+		this.name=name;
+		this.course= course;
+		this.phn = phn;
+	}
+	
+	public StudentConstructor(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public StudentConstructor()
+	{
+		
+	}
+	
+	@override
+	public class ConstructorDemo{
+		public ststic void main(String[] args)
+	}
+
+}
